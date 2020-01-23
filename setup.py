0c41b506a6a3068a1436458dc72ede9bac5e79de
Andrey Petukhov <a.petukhov@drweb.com>
2020-01-23 15:26:30 +0300

    dist
    Signed-off-by: Andrey Petukhov <a.petukhov@drweb.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+
+import pathlib
+import setuptools
 from distutils.core import (
     Extension,
     setup,
@@ -10,11 +13,23 @@ libsvm2csv_ext = Extension(
     language="c++",
 )
 
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 setup(
     name="libsvm2csv",
-    version="0.1.0",
-    description="Python SDK for libsvm2csv.",
+    version="0.1.1",
+    description="Convert LIBSVM data to csv format.",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Andrey Petukhov",
     author_email="andribas404@gmail.com",
     ext_modules=[libsvm2csv_ext],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+
 )
